5777f63be9474aca557b15288478a5b6c5d73929
Jay T <jaytula@gmail.com>
2020-08-05 17:19:18 -0700

    Core Concepts Using Middleware

--- lessons/core-concepts/7-using-middleware/functions/using-middleware.js
+++ lessons/core-concepts/7-using-middleware/functions/using-middleware.js
@@ -1,9 +1,35 @@
-exports.handler = (event, context, callback) => {
+const middy = require("middy");
+const { jsonBodyParser } = require("middy/middlewares");
+
+const myMiddleware = config => {
+  return {
+    before: (handler, next) => {
+      console.log("Before", handler.event.body);
+      next();
+    },
+    after: (handler, next) => {
+      console.log("After", handler.event.body);
+      next();
+    },
+    onError: (handler, next) => {
+      console.log("onError", handler);
+      next();
+    },
+  };
+};
+
+const businessLogic = (event, context, callback) => {
+  console.log(typeof event.body); // object
+
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      data: 'hello'
-    })
-  })
-}
+      data: "hello",
+      ...event.body
+    }),
+  });
+};
 
+exports.handler = middy(businessLogic)
+  .use(jsonBodyParser())
+  .use(myMiddleware({ foo: "bar" }));
