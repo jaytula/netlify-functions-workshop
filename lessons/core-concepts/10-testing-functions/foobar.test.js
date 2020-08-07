57d0c285d457ed7c4d17de581bd61e3089e14a9e
Jay T <jaytula@gmail.com>
2020-08-07 13:11:57 -0700

    Core Concepts Lesson 10: Testing Functions

--- lessons/core-concepts/10-testing-functions/foobar.test.js
+++ lessons/core-concepts/10-testing-functions/foobar.test.js
@@ -1,3 +1,10 @@
+const test = require('ava');
+const fetch = require('node-fetch');
+
 /* Step 7. In this_file, use `node-fetch` to call into your live URL to verify the response */
 test('return valid response', async t => {
+  const response = await fetch('http://localhost:8888/.netlify/functions/foobar');
+  const data = await response.json();
+  console.log(data);
+  t.assert(data.success, true);
 })
