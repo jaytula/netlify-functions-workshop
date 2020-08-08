3876216a8b90c5d567237a299ef4096b1da27d01
Jay T <jaytula@gmail.com>
2020-08-08 10:46:22 -0700

    Use Cases Lesson 02:: GraphQL

--- lessons/use-cases/2-graphql/functions/graphql.js
+++ lessons/use-cases/2-graphql/functions/graphql.js
@@ -1,10 +1,25 @@
 /* Step 3. In this_file, require 'apollo-server-lambda' */
+const { ApolloServer, gql } = require('apollo-server-lambda');
 
 /* Step 4. In this_file, define your graphQL types */
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
 /* Step 5. In this_file, define your graphQL resolvers */
+const resolvers = {
+  Query: {
+    hello: () => "hello worlld"
+  }
+};
 
 /* Step 6. In this_file, create a new `ApolloServer` and pass it your typeDefs and resolves */
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 /* Step 7. In this_file, turn your server into a lambda compatible function signature
 
@@ -14,3 +29,4 @@
 
     Then export the handler
 */
+exports.handler = server.createHandler();
