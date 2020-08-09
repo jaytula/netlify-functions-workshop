fe0e2a6cd6d26aba3f1527837f3e7816c9691834
Jay T <jaytula@gmail.com>
2020-08-09 09:01:11 -0700

    Use Cases Lesson 04: Setting Cookies

--- lessons/use-cases/4-setting-cookies/functions/set-cookie.js
+++ lessons/use-cases/4-setting-cookies/functions/set-cookie.js
@@ -2,6 +2,7 @@
 
     We will be using the [cookie](https://www.npmjs.com/package/cookie) module to set cookie headers in the response.
 */
+const cookie = require('cookie');
 
 exports.handler = async (event, context) => {
   /* Step 5. In this_file, use the cookie module to generate a new cookie string
@@ -15,32 +16,36 @@ exports.handler = async (event, context) => {
       const twoWeeks = 14 * 24 * hour
       ```
   */
+  const setCookie = cookie.serialize('foo', 'bar', {
+    maxAge: 60 * 60 * 24 * 14,
+    path: '/'
+  })
+
   /* Step 6. In this_file, choose a redirect URL to send visitors to after we set our cookie */
-  const redirectUrl = 'https://google.com'
+  const redirectUrl = 'https://www.engadget.com'
 
   /* Step 7. In this_file, we will be using html meta refresh as our redirect mechanism
 
     Because we are setting cookies from a `GET` request, we are returning back HTML to the browser to handle our final redirect.
 
-    ```js
-    const html = `
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <noscript>
-          <meta http-equiv="refresh" content="0; url=${redirectUrl}" />
-        </noscript>
-      </body>
-      <script>
-        setTimeout(function() {
-          window.location.href = ${JSON.stringify(redirectUrl)}
-        }, 0)
-      </script>
-    </html>`
-    ```
+
   */
+ const html = `
+ <html lang="en">
+   <head>
+     <meta charset="utf-8">
+   </head>
+   <body>
+     <noscript>
+       <meta http-equiv="refresh" content="0; url=${redirectUrl}" />
+     </noscript>
+   </body>
+   <script>
+     setTimeout(function() {
+       window.location.href = ${JSON.stringify(redirectUrl)}
+     }, 0)
+   </script>
+ </html>`
 
 
   /* Step 8. In this_file, return the response
@@ -52,4 +57,13 @@ exports.handler = async (event, context) => {
     Set the `headers['Content-Type']` to `'text/html'` and return our generated html in the `body`
 
   */
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Set-Cookie': setCookie,
+      'Content-Type': 'text/html'
+    },
+    body: html
+  }
 }
